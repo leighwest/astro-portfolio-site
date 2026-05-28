5f766eeb2e5adb51fb642c93ed11e81807d90373
Leigh West <leighwest@hotmail.com>
2026-05-28 21:40:08 +1000

    Added Open Graph meta tags for link previews

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,7 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://leighwest.dev',
   markdown: {
     shikiConfig: {
       theme: 'catppuccin-mocha' // soft pastels
